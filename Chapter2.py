884e033bab3208bce8dff425985908fce38eeb7a
Alper Ayna <40885180+Mad-Kingu@users.noreply.github.com>
2018-11-02 09:06:40 +0300

    Update Chapter2.py

--- Chapter2.py
+++ Chapter2.py
@@ -86,3 +86,57 @@ if __name__ == '__main__':
 	for i, v in enumerate(array):		      # for bigger number, highest value must be changed
 		print str(i) + " " + str(v)
 # End of Merge Sort
+
+# Exercise 2.3-2 
+# Start of Merge Sort Without Sentinels
+import os
+import re
+import math
+import random
+def merge_sort(array, p, r):
+	if p < r:
+		q = int((p + r)/2) #math int flooring the value
+		print ("p: %s q:%s r:%s" % (p, q, r))
+		merge_sort(array, p, q)
+		merge_sort(array, q+1, r)
+		merge(array, p, q, r)
+
+def merge(array, p, q, r):
+	n1 = q - p + 1
+	n2 = r - q
+	L = [None] * (n1)
+	R = [None] * (n2)
+	for i in range(0, n1):
+		L[i] = array[p + i]
+	for j in range(0, n2):
+		R[j] = array[q + j + 1]
+	i = 0
+	j = 0
+	for k in range(p, r+1):
+		if len(L) > i and len(R) > j:
+			if L[i] <= R[j]:
+				array[k] = L[i]
+				i += 1
+			else:
+				array[k] = R[j]
+				j += 1
+		elif len(L) > i:
+			array[k] = L[i]
+			i += 1
+		elif len(R) > j:
+			array[k] = R[j]
+			j += 1
+
+def testcase():
+	array = []
+	array_range = 100
+	for x in range(array_range):
+		array.append(random.randint(1, 1000))
+	return array
+
+if __name__ == '__main__':
+	array = testcase()
+	merge_sort(array, 0, array.__len__() - 1)
+	for i, v in enumerate(array):
+		print str(i) + " " + str(v)
+# End of Merge Sort Without Sentinels
