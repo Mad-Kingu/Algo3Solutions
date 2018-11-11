8f4ff9387d24c56a5615c88396cf810850f060fa
Alper Ayna <40885180+Mad-Kingu@users.noreply.github.com>
2018-11-11 16:29:50 +0300

    Update Chapter2.py

--- Chapter2.py
+++ Chapter2.py
@@ -1,34 +1,21 @@
 #Solutions
 #Python version 2.7
-#Every code between start and end tag works standalone
-#Some of the test cases are very simple because of my lazyness
-
-# Start Simple Insertion Sort
 import os
 import re
 import math
+import random
+import time
 
 def insertion_sort(array):
 	for j, v in enumerate(array):
 		key = v
 		i = j - 1
-		while i > 0 and array[i] > key:
+		while i > -1 and array[i] > key:
 			array[i+1] = array[i]
 			i = i - 1
 		array[i+1] = key
-		
-	for v in enumerate(array):
-		print v
-    
-    
-if __name__ == '__main__':
-	insertion_sort([2,3,7,5])
-# End of Simple Insertion Sort
-
-# Start Reversed Insertion Sort
-import os
-import re
-import math
+			
+			
 def reverse_insertion_sort(array):
 	for j, v in enumerate(array):
 		key = v
@@ -38,28 +25,14 @@ def reverse_insertion_sort(array):
 			i = i - 1
 		array[i+1] = key
 		
-	for v in enumerate(array):
-		print v
-    
-    
-if __name__ == '__main__':
-	reverse_insertion_sort([2,3,7,5])
-# End of Reversed Insertion Sort
-
-# Start Merge Sort
-import os
-import re
-import math
-import random
-
-def merge_sort(array, p, r):
+def merge_sort_with_sentinel(array, p, r):
 	if p < r: 
 		q = int((p + r)/2) #math int flooring the value
-		merge_sort(array, p, q)
-		merge_sort(array, q+1, r)
-		merge(array, p, q, r)
+		merge_sort_with_sentinel(array, p, q)
+		merge_sort_with_sentinel(array, q+1, r)
+		merge_with_sentinel(array, p, q, r)
 	
-def merge(array, p, q, r):
+def merge_with_sentinel(array, p, q, r):
 	n1 = q - p + 1
 	n2 = r - q
 	L = [None] * (n1 + 1)
@@ -68,8 +41,8 @@ def merge(array, p, q, r):
 		L[i] = array[p + i]
 	for j in range(0, n2):
 		R[j] = array[q + j + 1]
-	L[n1] = 1023
-	R[n2] = 1023
+	L[n1] = 1023				#this and
+	R[n2] = 1023				#this are the sentinels
 	i = 0
 	j = 0
  	for k in range(p, r+1):
@@ -79,26 +52,10 @@ def merge(array, p, q, r):
 		else:
 			array[k] = R[j]
 			j += 1
-
-if __name__ == '__main__':
-	array = []
-	array_range = 100
-	for x in range(array_range):
-		array.append(random.randint(1, 1000)) # just for random samples from random generation
-	for i, v in enumerate(array):		      # for bigger number, highest value must be changed
-		print str(i) + " " + str(v)
-# End of Merge Sort
-
-# Exercise 2.3-2 
-# Start of Merge Sort Without Sentinels
-import os
-import re
-import math
-import random
+		
 def merge_sort(array, p, r):
 	if p < r:
 		q = int((p + r)/2) #math int flooring the value
-		print ("p: %s q:%s r:%s" % (p, q, r))
 		merge_sort(array, p, q)
 		merge_sort(array, q+1, r)
 		merge(array, p, q, r)
@@ -129,6 +86,41 @@ def merge(array, p, q, r):
 			array[k] = R[j]
 			j += 1
 
+
+def recursive_insertion_sort(array, p, q):
+	if q > 0:
+		q = q - 1
+		recursive_insertion_sort(array, p, q)
+		recursive_insertion_merge(array, p, q)
+
+def recursive_insertion_merge(array, p, q):
+	i = 0
+	while i < q:
+		if array[i+2] < array[i+1] and array[i] < array[i+2]:
+			array[i+1], array[i+2] = swap(array[i+1], array[i+2])
+			if i > 2:
+				i -= 3
+			elif i > 1:
+				i -= 2
+		elif array[i + 2] < array[i]:
+			array[i+2], array[i] = swap(array[i+2], array[i])
+			if array[i+1] > array[i+2]:
+				array[i+1], array[i+2] = swap(array[i+1], array[i+2])
+			if i > 2:
+				i -= 3
+			elif i > 1:
+				i -= 2
+		elif array[i + 2] > array[i+1]:
+			i += 1
+			continue
+		i += 1
+			
+def swap(a,b):
+	temp = a
+	a = b
+	b = temp
+	return a, b
+	
 def testcase():
 	array = []
 	array_range = 100
@@ -137,9 +129,57 @@ def testcase():
 	return array
 
 if __name__ == '__main__':
-	array = testcase()
-	merge_sort(array, 0, array.__len__() - 1)
-	for i, v in enumerate(array):
-		print str(i) + " " + str(v)
-# End of Merge Sort Without Sentinels
+	print "timetest begins"
+	
+	array1 = testcase()
+	array = array1
 
+	t0 = time.clock()
+	insertion_sort(array)
+	t1 = time.clock()
+	print "insertion sort: " + str(t1-t0)
+	for i, v in enumerate(array):
+		print " " + str(v),
+	print ""
+
+	array = array1
+	
+	t0 = time.clock()
+	reverse_insertion_sort(array)
+	t1 = time.clock()
+	print "reverse insertion sort: " + str(t1-t0)
+	for i, v in enumerate(array):
+		print " " + str(v),
+	print ""
+
+	array = array1	
+	
+	t0 = time.clock()
+	merge_sort_with_sentinel(array, 0, array.__len__() - 1)
+	t1 = time.clock()
+	print "merge_sort_with_sentinel: " + str(t1-t0)
+	for i, v in enumerate(array):
+		print " " + str(v),
+	print ""
+
+	array = array1
+	
+	t0 = time.clock()
+	merge_sort(array, 0, array.__len__() - 1)
+	t1 = time.clock()
+	print "merge_sort: " + str(t1-t0)
+	for i, v in enumerate(array):
+		print " " + str(v),
+	print ""
+
+	array = array1
+	
+	t0 = time.clock()
+	recursive_insertion_sort(array, 0, array.__len__() - 1)
+	t1 = time.clock()
+	print "recursive_insertion_sort: " + str(t1-t0)
+	for i, v in enumerate(array):
+		print " " + str(v),
+	print ""
+
+#Test results for 100 integer
