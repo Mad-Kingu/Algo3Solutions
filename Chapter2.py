cf83ff22b02f62cce8a8ceb0cf845ef29e26d545
Alper Ayna <40885180+Mad-Kingu@users.noreply.github.com>
2018-11-19 01:01:09 +0300

    Update Chapter2.py

--- Chapter2.py
+++ Chapter2.py
@@ -244,14 +244,15 @@ def insertion_sort(array):
 		
 def insertion_sort_v2(array):
 	for j, v in enumerate(array):
-		key = v
-		if j > 3:
+		if j > 0:
+			key = array[j]
 			a = binary_search(array, key, j)
-			array.insert(a, key)
-			array.pop(a+1)
+			for i in range(j, a, -1):
+				array[i] = array[i-1]
+			array[a] = key
 
 def binary_search(array, searchingelement, arraypart):
-	array = array[:arraypart]
+	array = list(array[:arraypart])
 	last = array.__len__()
 	mid = int(last/2)
 	min = 0
@@ -264,28 +265,42 @@ def binary_search(array, searchingelement, arraypart):
 		else:
 			last = mid
 			mid = int((mid + min) / 2)
-	return mid
+	if array[mid] < searchingelement:
+		return mid+1
+	elif array[mid] > searchingelement:
+		if mid-1 > -1:
+			return mid-1
+		else:
+			return mid
+	else:
+		return mid
+	
 	
 if __name__ == '__main__':
 	array1 = []
-	for i in range(1000):
+	for i in range(10000):
 		array1.append(randint(0, 1000))
 	
-	array = array1
+	array = list(array1)
 	t0 = time.clock()
 	insertion_sort(array)
 	t1 = time.clock()
 	print "insertion_sort: " + str(t1-t0)
-			
-	array = array1
+
+	array = list(array1)
 	t0 = time.clock()
 	insertion_sort_v2(array)
 	t1 = time.clock()
 	print "insertion_sort_v2: " + str(t1-t0)
-# Test results shows that worst case of improved insertion sort is O(n * lg(n)) 
+# Test results shows that worst case of improved insertion sort is O(n * (n\2) * lg(n)) 
+# Better than insertion sort but still very bad
 # Tested for 1000 random elements
-# insertion_sort:----0.0393522697063
-# insertion_sort_v2:-0.00700267815824
+# insertion_sort:----0.0390096090178
+# insertion_sort_v2:-0.0287921815039
+
+# Tested for 10000 random elements
+# insertion_sort:----3.76619711492
+# insertion_sort_v2:-2.25984142782
 # End of 2.3-6 in book
 
 
